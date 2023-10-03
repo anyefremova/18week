40f1766a613c651931ce7760758a88f422b26ea7
anyefremova <anna.efremova.02@yandex.ru>
2023-10-04 00:00:48 +0500

    html

---
+++ task 2/index.js
@@ -0,0 +1,5 @@
+const input = document.getElementById('input_task');
+const taskText = input.value;
+
+const addButton = document.getElementsByClassName('btn__add');
+const clearButton = document.getElementsByClassName('btn__clear');

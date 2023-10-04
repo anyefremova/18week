55384ad582decd207b66c451296cc304df4300bb
anyefremova <anna.efremova.02@yandex.ru>
2023-10-05 01:37:41 +0500

    js

--- task 2/index.js
+++ task 2/index.js
@@ -3,3 +3,10 @@ const taskText = input.value;
 
 const addButton = document.getElementsByClassName('btn__add');
 const clearButton = document.getElementsByClassName('btn__clear');
+
+const list = document.getElementsByClassName('list');
+const listTask = document.getElementsByClassName('list__task');
+
+function makeTask() {
+
+}
